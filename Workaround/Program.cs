e2457011305048792fcda967690a312c94fa149f
esrabetulozcann <103575815+esrabetulozcann@users.noreply.github.com>
2024-02-14 13:23:10 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Degiskenler();
+
+            Vatandas vatandas1 = new Vatandas();
+
+            SelamVer("Engin");
+            SelamVer("Ahmet");
+            SelamVer("Ayşe");
+            SelamVer();
+
+            int sonuc = Topla(6,58);
+
+            //Diziler / Arrays
+
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Berkay";
+
+            Console.WriteLine(ogrenci1);
+            Console.WriteLine(ogrenci2);
+            Console.WriteLine(ogrenci3);
+
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Berkay";
+            
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "İlker";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]); // Adana olacak.
+
+
+            Person person1 = new Person();
+            
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+        //resharper
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc.ToString());
+            return sonuc;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 10000; // db'den gelir.
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            int dogumYili = 1985;
+            long tcNo = 112345678910;
+
+
+
+            Console.WriteLine(tutar * 1.18);
+
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
